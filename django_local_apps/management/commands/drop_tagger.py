e623e20b0fa2972dc11aae45c7bc744f49d0957c
Richard <richardwangwang@gmail.com>
2015-11-21 10:25:08 +0800

    Adding tagging function

--- django_local_apps/management/commands/drop_tagger.py
+++ django_local_apps/management/commands/drop_tagger.py
@@ -1,4 +1,7 @@
+from django_local_apps.server_configurations import get_ufs_web_server_port, get_admin_username, get_admin_password
+from iconizer.gui_client.browser_service_client import BrowserServiceClass
 from iconizer.iconizer_consts import ICONIZER_SERVICE_NAME
+from libtool.string_tools import quote_unicode
 from universal_clipboard.management.commands.cmd_handler_base.msg_process_cmd_base import MsgProcessCommandBase
 
 __author__ = 'weijia'
@@ -13,8 +16,15 @@ class DropTagger(MsgProcessCommandBase):
     def process_msg(self, msg):
         if self.is_need_process(msg):
             if "urls" in msg:
-                print msg["urls"]
-                # self.platform_service.open_browser()
+                links = ""
+                for i in msg["urls"]:
+                    links += "url=" + quote_unicode(unicode(i)) + "&"
+                tagging_url = "http://127.0.0.1:%s/webmanager/login_and_go_home/?" \
+                         "username=%s&password=%s&target=" \
+                         "/obj_sys/tagging_local/?%s" % (
+                         str(get_ufs_web_server_port()), get_admin_username(), get_admin_password(), links)
+                print tagging_url
+                BrowserServiceClass().open_browser(tagging_url)
 
     def register_to_service(self):
         channel = self.get_channel("drop_target_channel")
---
+++ django_local_apps/server_configurations.py
@@ -0,0 +1,15 @@
+from djangoautoconf.local_key_manager import get_local_key
+
+__author__ = 'weijia'
+
+
+def get_ufs_web_server_port():
+    return 8110
+
+
+def get_admin_username():
+    return get_local_key("admin_account.admin_user")
+
+
+def get_admin_password():
+    return get_local_key("admin_account.admin_password")
