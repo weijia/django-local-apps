e60a3d9edf4ef6bbde8093a3c755064a76bab36c
Richard <richardwangwang@gmail.com>
2015-11-21 12:32:48 +0800

    Use obj_sys client.

--- django_local_apps/management/commands/drop_tagger.py
+++ django_local_apps/management/commands/drop_tagger.py
@@ -2,6 +2,7 @@ from django_local_apps.server_configurations import get_ufs_web_server_port, get
 from iconizer.gui_client.browser_service_client import BrowserServiceClass
 from iconizer.iconizer_consts import ICONIZER_SERVICE_NAME
 from libtool.string_tools import quote_unicode
+from obj_sys.obj_sys_client import ObjSysClint
 from universal_clipboard.management.commands.cmd_handler_base.msg_process_cmd_base import MsgProcessCommandBase
 
 __author__ = 'weijia'
@@ -19,11 +20,13 @@ class DropTagger(MsgProcessCommandBase):
                 links = ""
                 for i in msg["urls"]:
                     links += "url=" + quote_unicode(unicode(i)) + "&"
-                tagging_url = "http://127.0.0.1:%s/webmanager/login_and_go_home/?" \
-                         "username=%s&password=%s&target=" \
-                         "/obj_sys/tagging_local/?%s" % (
-                         str(get_ufs_web_server_port()), get_admin_username(), get_admin_password(), links)
-                print tagging_url
+
+                c = ObjSysClint()
+                c.password = get_admin_password()
+                c.server_port = str(get_ufs_web_server_port())
+                c.username = get_admin_username()
+                tagging_url = c.get_manual_tagging_url(links)
+                # print tagging_url
                 BrowserServiceClass().open_browser(tagging_url)
 
     def register_to_service(self):
