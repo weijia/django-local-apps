9ffb8dc60890611bdb5fb9155636f76bebd57a6a
Richard <richardwangwang@gmail.com>
2016-02-02 23:27:17 +0800

    Added interface enumerate. Added pydht.

--- django_local_apps/management/commands/local_app_utils/host_detection.py
+++ django_local_apps/management/commands/local_app_utils/host_detection.py
@@ -1,5 +1,26 @@
+import netifaces as ni
 from socket import *
+from time import sleep
+
+# from PyQt4.QtNetwork import QNetworkInterface
+#
+# all_Addresses = QNetworkInterface.allAddresses()    #list-of-QHostAddress
+#
+# for addr in all_Addresses:
+#     print(addr.toString())
+
+
+for i in ni.interfaces():
+    interface = ni.ifaddresses(i)
+    if 2 in interface:
+        print interface
+        print interface[2][0]["addr"]
+
+
 cs = socket(AF_INET, SOCK_DGRAM)
+cs.bind(("192.168.8.120", 0))
 cs.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 cs.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-cs.sendto('This is a test', ('255.255.255.255', 988))
+# while True:
+#     cs.sendto('This is a test', ('255.255.255.255', 988))
+#     sleep(10)
