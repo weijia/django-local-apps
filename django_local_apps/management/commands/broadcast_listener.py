3f6e024052dae7d3800c341da156037e719ef476
Richard <richardwangwang@gmail.com>
2016-02-02 22:52:55 +0800

    Added broadcast_listener.py.

---
+++ django_local_apps/management/commands/broadcast_listener.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+from socket import *
+from django.core.management import BaseCommand
+
+
+class BroadcastListener(BaseCommand):
+    def handle(self, *args, **options):
+        cs = socket(AF_INET, SOCK_DGRAM)
+        cs.bind(('', 988))
+        while True:
+            data, address = cs.recvfrom(1024)
+            print ':received:', data, "from ", address
+            if data == "HLK":
+                cs.sendto("HLK-RM(V1.78(Jul 23))", address)
+
+
+Command = BroadcastListener
