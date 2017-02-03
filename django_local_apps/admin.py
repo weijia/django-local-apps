fb20b62811ca315e08183b54552b32ddf9ab12a6
Richard <richardwangwang@gmail.com>
2017-02-03 23:10:17 +0800

    Added sort for content type field drop down list.

--- django_local_apps/admin.py
+++ django_local_apps/admin.py
@@ -1,11 +1,23 @@
+from djangoautoconf.auto_conf_admin_tools.admin_features.foreign_key_sort import ForeignKeySortFeature
 from djangoautoconf.auto_conf_admin_tools.admin_register import AdminRegister
 from djangoautoconf.auto_conf_admin_tools.foreign_key_auto_complete import ForeignKeyAutoCompleteFeature
 from obj_sys.models_ufs_obj import UfsObj
 import models
 
+
+r = AdminRegister()
+
+s = ForeignKeySortFeature()
+s.sort_attribute = "content_type"
+s.sort_field_of_foreign_key = "model"
+r.add_feature(s)
+r.register(models.DbCleanConfig)
+
+
 r = AdminRegister()
 f = ForeignKeyAutoCompleteFeature()
 f.set_search_field_by_model({UfsObj: ("uuid", "ufs_url", "full_path")})
 r.add_feature(f)
+s = ForeignKeySortFeature()
 r.register_all_models(models)
 
