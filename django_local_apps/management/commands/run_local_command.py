fe5015cdcfca8262c89982a2be2e30ffa74770d3
richard <richardwangwang@gmail.com>
2017-02-04 17:28:10 +0800

    Update parameter for run_local_command.py

--- django_local_apps/management/commands/run_local_command.py
+++ django_local_apps/management/commands/run_local_command.py
@@ -2,6 +2,8 @@ import logging
 import os
 
 import sys
+from optparse import make_option
+
 from pinax.eventlog.models import Log
 
 from django_local_apps.management.commands.local_app_utils.db_clean_utils import remove_expired_record
@@ -13,9 +15,12 @@ log = logging.getLogger()
 
 class LocalCommandRunner(DjangoCmdBase):
 
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('path', nargs='+')
+
     def msg_loop(self):
-        print sys.argv
-        os.system(sys.argv[2])
+        print self.options["path"]
 
 
 Command = LocalCommandRunner
