5a99052946085ad9f7440700f58247d1ae94d936
richard <richardwangwang@gmail.com>
2017-07-13 11:56:48 +0800

    Fix time query issue.

--- django_local_apps/management/commands/local_app_utils/db_clean_utils.py
+++ django_local_apps/management/commands/local_app_utils/db_clean_utils.py
@@ -3,6 +3,13 @@ from django.utils import timezone
 
 
 def remove_expired_record(expire_days, query_set, time_attr_name="timestamp"):
-    expired_record_filter = {"%s_lt" % time_attr_name: timezone.now() - timezone.timedelta(days=expire_days)}
+    expired_record_filter = {"%s__lt" % time_attr_name: timezone.now() - timezone.timedelta(days=expire_days)}
     q = Q(**expired_record_filter)
-    query_set.filter(q).delete()
+    final_q = query_set.filter(q)
+    # cnt = 0
+    # for i in final_q:
+    #     i.delete()
+    #     cnt += 1
+    #     if cnt % 1000 == 0:
+    #         print "%d deleted" % cnt
+    final_q.delete()
