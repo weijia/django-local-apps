e3505746e0f09c103fd875a24ded85290272cfb9
Richard <richard.j.wang@nokia-sbell.com>
2018-12-13 11:21:24 +0800

    Print execution result for docker.

--- django_local_apps/management/commands/docker_exec.py
+++ django_local_apps/management/commands/docker_exec.py
@@ -23,7 +23,7 @@ class DockerExecutor(DjangoCmdBase):
         print(self.options["path_and_params"])
         client = docker.from_env()
         container = client.containers.get(self.options["container_id"][0])
-        container.exec_run(" ".join(self.options["path_and_params"]), workdir=self.options["work_dir"])
+        print(container.exec_run(" ".join(self.options["path_and_params"]), workdir=self.options["work_dir"]))
 
 
 Command = DockerExecutor
